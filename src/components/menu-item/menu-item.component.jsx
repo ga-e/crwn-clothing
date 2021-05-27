b6dd35175b7fa2e612fe3e50776dfe9ba597bb95
ga-e <gauravhunk2013@gmail.com>
2021-05-27 14:35:27 +0530

    updated navigation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './menu_item.styles.scss';
 
-const MenuItem=({title,imageUrl,size})=>
+const MenuItem=({title,imageUrl,size,history,linkUrl,match})=>
 (
     <div  
-    className= {`${size} menu-item`}>
+    className= {`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
                 <div className='background-image' style={{background:`url(${imageUrl})`}}   />
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-)
+);
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import '../../components/directory/directory.component'
 import Directory from '../../components/directory/directory.component';
 const HomePage=() => (
     <div className='homepage'>
-        <Directory/>
+        <Directory />
 
         </div>
         
